dc27397c5d7d402010dcd78b91d9492bdd1f48eb
spandana <spandana@gmail.com>
2023-09-25 15:17:15 +0530

    terra

--- main.tf
+++ main.tf
@@ -2,40 +2,39 @@ provider "aws" {
  region = "ap-south-1"
 }
 
-resource "aws_instance" "kelly" {
+resource "aws_instance" "kellydevops" {
   ami = "ami-0f5ee92e2d63afc18"
 instance_type = "t2.micro"
 
 tags = {
-  Name = "terraform"
+  Name = "instance"
 }
 }
 
-resource "aws_key_pair" "dev" {
+resource "aws_key_pair" "deva" {
   key_name = "key"
   public_key = file("./key.pub")
 }
 
-resource "aws_vpc" "maha" {
-  cidr_block = "10.10.0.0/16"
+resource "aws_vpc" "shiv" {
+  cidr_block = "172.10.0.0/16"
   tags = {
-    Name = "my vpc"
+    Name = "vpc"
   } 
 }
 
 resource "aws_subnet" "subnet1" {
-  cidr_block = "10.10.1.0/24"
-  vpc_id = "vpc-0ffb248944fb413bf"
+  cidr_block = "172.10.1.0/24"
+  vpc_id = aws_vpc.shiv.id
   tags = {
-    Name = "s1"
+    Name = "subnet1"
   }
 }
 
 resource "aws_subnet" "subnet2" {
-  cidr_block = "10.10.2.0/24"
-  vpc_id = "vpc-0ffb248944fb413bf"
+  cidr_block = "172.10.2.0/24"
+  vpc_id = aws_vpc.shiv.id
   tags = {
-    Name = "s2"
+    Name = "subnet2"
   }
 }
-
